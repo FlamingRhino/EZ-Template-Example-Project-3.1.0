89702cef6a8832b4087bdf6b7172b04846619951
FlamingRhino <rvankirk27@setoncatholic.org>
2024-11-07 19:02:38 -0700

    eeeeeeeeeeeee

--- src/autons.cpp
+++ src/autons.cpp
@@ -492,6 +492,19 @@ void goalrush(){
   chassis.pid_turn_set(-45, TURN_SPEED);
   chassis.pid_wait();
 
+  chassis.pid_drive_set(-3_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+  Piston11.set(true);
+
+  chassis.pid_turn_set(-180, TURN_SPEED);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(8_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+
+
 
 }
 
