23d977f3c114205ceb6c49cde5f69c2686d374a0
FlamingRhino <rvankirk27@setoncatholic.org>
2024-12-29 15:47:22 -0700

    should work

--- src/arm.cpp
+++ src/arm.cpp
@@ -3,6 +3,13 @@
 
  inline ez::PID armPID{0.45, 0, 0, 0, "armp"};
 
+ 
+inline void set_lift(int input) {
+  l_arm.move(input);
+  r_arm.move(input);
+}
+
+
  inline void armtask(){
 
     pros::delay(2000);  // Set EZ-Template calibrate before this function starts running
@@ -12,12 +19,6 @@
       pros::delay(ez::util::DELAY_TIME);
     }
 
-}
-
-
-inline void set_lift(int input) {
-  l_arm.move(input);
-  r_arm.move(input);
 }
 
 
